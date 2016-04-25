d1650a7e443440b629229156ba1277b5c420934a
cherylgao <cheryl.cgao@gmail.com>
2016-04-25 11:52:01 -0700

    updated on remove noise

--- java/src/ContentExtractor.java
+++ java/src/ContentExtractor.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -13,6 +15,7 @@ public class ContentExtractor {
 	Document doc;
 	HashMap<Tag, ArrayList<TagInfo>> map;
 	String title;
+	Elements elements;
 	//TODO: need to iterate over repository to get all files
 
 	public ContentExtractor(String path){
@@ -24,22 +27,26 @@ public class ContentExtractor {
 		title=doc.select("title").text();
 	}
 	public void bodyProcessor(Document doc){
-		//filter the doc
-		String html = doc.html();
-		String clean=Jsoup.clean(html, Whitelist.basic());
-		//remove extra spaces
-		clean=clean.replaceAll("&nbsp", " ");
+//		//filter the doc
+//		String html = doc.html();
+//		String clean=Jsoup.clean(html, Whitelist.basic());
+//		//remove extra spaces
+//		clean=clean.replaceAll("&nbsp", " ");
 		
-		System.out.println(clean); //for testing
+//		System.out.println(clean); //for testing
 		
-		Elements elements=doc.body().select("*");
-		for(Element e: elements){
-			Tag tag=e.tag();
+		doc.select("script,noscript,style,iframe,br").remove(); //from webCollector on github
+		
+		elements=doc.body().select("*");
+		
+		for(int i=0;i<elements.size();i++){
+			Tag tag=elements.get(i).tag();
 			TagInfo taginfo= new TagInfo(tag);
-			String context=e.text();
+			String context=elements.get(i).text();
 			taginfo.setContent(context);//difference between ownText() and text()?
 			int len=context.length();
 			taginfo.setLength(len);
+			taginfo.position=i;
 			if(map.containsKey(tag)){
 				ArrayList<TagInfo> arr= map.get(tag);
 				arr.add(taginfo);
@@ -54,9 +61,20 @@ public class ContentExtractor {
 	}
 	void removeNoise(){
 		//TODO: remove noise based on the length of content under each tag
+		Iterator<Map.Entry<Tag, ArrayList<TagInfo>>> it=map.entrySet().iterator();
+		while(it.hasNext()){
+			Map.Entry<Tag, ArrayList<TagInfo>> pair=it.next();
+			ArrayList<TagInfo> arr=pair.getValue();
+			for(TagInfo entry:arr){
+				if(entry.getLength()<4){ //just come up with this number, need more discuss on that
+					int index=entry.getPos();
+					elements.get(index).text(""); //should be able to set text to "", not sure about this yet
+				}
+			}
+		}
 	}
 	void finalContent(){
-		//TODO: generate final text and output title and body to file
+		//TODO: generate final text and output title and processed body to file
 	}
 
 }
