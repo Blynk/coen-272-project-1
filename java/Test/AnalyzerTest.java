841ab8a959e4dc4bf05f634e5a7665077079322f
Blynk <david.obatake@gmail.com>
2016-04-25 17:19:39 -0700

    Part 3 working

---
+++ java/Test/AnalyzerTest.java
@@ -0,0 +1,10 @@
+/**
+ * Created by dobatake on 4/25/16.
+ */
+public class AnalyzerTest {
+    public static void main(String[] args) {
+        WordAnalyzer wa = new WordAnalyzer();
+
+        wa.run();
+    }
+}
