f05e63157797d7953dc67dfc38e528a53d212270
Blynk <david.obatake@gmail.com>
2016-04-26 09:42:07 -0700

    Changed path for files to use absolute path

--- java/src/ContentManager.java
+++ java/src/ContentManager.java
@@ -3,17 +3,22 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import static java.nio.file.Paths.get;
+
 public class ContentManager {
 	private ArrayList<Path> docStoreList;
     private Path docStore;
 	
 	public ContentManager(){
-		this.docStore = Paths.get("").toAbsolutePath();
-        this.docStore = Paths.get(this.docStore.toString(), "HTMLDocStore");
+		this.docStore = get("").toAbsolutePath();
+        this.docStore = get(this.docStore.toString(), "HTMLDocStore");
         this.docStoreList = DirectoryIterator.getHTMLFiles(this.docStore);
         
         for(Path p:docStoreList){
-        	String path=p.toString();
+            // docStoreList only contains name of the file, not absolute path to file
+            // So we append the name to the path of the HTMLDocStore directory
+            Path filepath = Paths.get(this.docStore.toString(), p.toString());
+        	String path=filepath.toString();
         	ContentExtractor ce= new ContentExtractor(path);
         	ce.bodyProcessor();
         }
